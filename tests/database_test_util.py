b0a1d40ef398ff06c6e05785b9a4c6866616eaa3
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-09-08 17:59:02 +0900

    [Fix] コメントを追加

--- tests/database_test_util.py
+++ tests/database_test_util.py
@@ -7,6 +7,13 @@ def setup_load_csv(table, filepath, truncate=True):
     """
     load_csv()を実行するデコレータです.
     ※load_csv()とは違いコミットします
+
+    使用時の注意事項
+        テストメソッドのトランザクションとデコレータのトランザクションが異なるため、
+        デコレータ実行時にテストメソッドのトランザクションが開始されていると、
+        metadata lockが発生し、DB更新処理で止まってしまい、終了しなくなる可能性があります。
+
+        metadata lockの最も簡単な回避策は、setUp()でcommit()またはrollback()を行うことです。
     """
     def _setup_load_csv(func):
         # 関数名がデコレータで上書きされてしまうのを防ぐ
